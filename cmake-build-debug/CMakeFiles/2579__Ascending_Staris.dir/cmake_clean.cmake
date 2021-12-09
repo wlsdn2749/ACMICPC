78274869ba71d828af3809727d06d80ef4fd3f5d
Jinwoo Jung <wlsdn2749@naver.com>
2021-12-09 14:57:18 +0900

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/2579__Ascending_Staris.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "2579__Ascending_Staris"
+  "2579__Ascending_Staris.pdb"
+  "CMakeFiles/2579__Ascending_Staris.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/2579__Ascending_Staris.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
